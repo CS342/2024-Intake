09b78d8145e58c6744a0cc1e6f6a6be2a7babfb4
nriedman <108841122+nriedman@users.noreply.github.com>
2024-02-02 17:06:14 -0800

    LLM Finish

--- Intake/ChiefComplaint/LLMInteraction.swift
+++ Intake/ChiefComplaint/LLMInteraction.swift
@@ -17,11 +17,74 @@ import SpeziLLMOpenAI
 import SwiftUI
 
 struct LLMInteraction: View {
+    @State private var chiefComplaint: String? = "blah blah blah"
+    
+    struct SummarizeFunction: LLMFunction {
+        static let name: String = "summarize_complaint"
+        static let description: String = """
+                    When there is enough information to give to the doctor,\
+                    summarize the conversation into a concise Chief Complaint.
+                    """
+        
+        @Parameter(description: "The primary medical concern that the patient is experiencing.") var medicalConcern: String
+        
+        @Parameter(description: "The severity of the primary medical concern.") var severity: String
+        
+        @Parameter(description: "The duration of the primary medical concern.") var duration: String
+        
+        @Parameter(description: "Extra important information relevant to the primary medical concern that the doctor should be aware of.")
+        var supplementaryInfo: String
+        
+        @Binding var chiefComplaint: String
+        
+        func execute() async throws -> String? {
+            let summary = """
+            Here is the summary that will be provided to your doctor:\n
+                Primary concern: \(medicalConcern)\n
+                Severity: \(severity)\n
+                Duration: \(duration)\n
+                Extra Info: \(supplementaryInfo)\n
+            """
+            chiefComplaint = summary
+            return summary
+        }
+    }
+    
+    @State private var shouldNavigateToSummaryView = true
     @Binding var presentingAccount: Bool
     @Environment(LLMRunner.self) var runner: LLMRunner
     
     @State var responseText: String
     @State var showOnboarding = true
+//    @State var model: LLM
+    
+//    init() {
+//        model = LLMOpenAI(
+//            parameters: .init(
+//                modelType: .gpt3_5Turbo,
+//                systemPrompt: """
+//                    You are acting as an intake person at a clinic and need to work with\
+//                    the patient to help clarify their chief complaint into a concise,\
+//                    specific complaint.
+//                
+//                    You should always ask about severity and duration if the patient does not include this information.
+//                
+//                    Additionally, help guide the patient into providing information specific to the condition that the define.\
+//                    For example, if the patient is experiencing leg pain, you should prompt them to be more\
+//                    specific about laterality and location. You should also ask if the pain is dull or sharp,\
+//                    and encourage them to rate their pain on a scale of 1 to 10. For a cough, for example, you\
+//                    should inquire whether the cough is wet or dry, as well as any other characteristics of the\
+//                    cough that might allow a doctor to rule out diagnoses.
+//                
+//                    Please use everyday layman terms and avoid using complex medical terminology.\
+//                    Only ask one question or prompt at a time, and keep your responses brief (one to two short sentences).
+//                """
+//            )
+//        )
+//        $model.function = SummarizeFunction(ChiefComplaint: $ChiefComplaint)
+//    }
+    
+    
     @State var model: LLM = LLMOpenAI(
         parameters: .init(
             modelType: .gpt3_5Turbo,
@@ -43,7 +106,9 @@ struct LLMInteraction: View {
                 Only ask one question or prompt at a time, and keep your responses brief (one to two short sentences).
             """
         )
-    )
+    ) {
+//        SummarizeFunction(ChiefComplaint: $ChiefComplaint)
+    }
     
     var body: some View {
         NavigationStack {
@@ -65,40 +130,24 @@ struct LLMInteraction: View {
                         let stream = try await runner(with: model).generate(prompt: """
                                         Hello! I am a patient coming in to see the doctor and would like\
                                         to discuss the reason for my visit.
-                                        """)
-   
+                                    """)
                         var isFirstToken = true
-                        
                         for try await token in stream {
-                            print(token)
                             if isFirstToken {
                                 isFirstToken = false
-                                continue
-                            }
-                            model.context.append(assistantOutput: token)
-                        }
-                    }
-                }
-            }
-        }
-    }
-    
-    func executePrompt(prompt: String) async {
-        // Execute the query on the runner, returning a stream of outputs
-        let stream = try? await runner(with: model).generate(prompt: prompt)
-        
-        if let unwrappedStream = stream {
-            do {
-                for try await token in unwrappedStream {
-                    responseText.append(token)
-                }
-            } catch {
-                // Handle any errors that occurred during the asynchronous operation
-                print("Error: \(error)")
-            }
-        }
-    }
-}
+                                continue }
+                            model.context.append(assistantOutput: token)}}}}
+            .onChange(of: chiefComplaint) { _, newChiefComplaint in
+                if let newChiefComplaint = newChiefComplaint {
+                    shouldNavigateToSummaryView = true}}
+            .background(
+                NavigationLink(
+                    destination: SummaryView(chiefComplaint: chiefComplaint ?? "error"),
+                    isActive: $shouldNavigateToSummaryView
+                ) { EmptyView() }
+                .isDetailLink(false)
+                .navigationDestination(isPresented: $shouldNavigateToSummaryView) {
+                    EmptyView() })}}}
 
 
 #Preview {
