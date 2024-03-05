aa521da2aed670c9f4a0e9e932298918e3ebe20c
Nina Boord <86579493+ninaboord@users.noreply.github.com>
2024-03-04 21:23:38 -0800

    pdf view

--- Intake/Export/ExportView.swift
+++ Intake/Export/ExportView.swift
@@ -6,3 +6,45 @@
 //
 
 import Foundation
+import UIKit
+import PDFKit
+
+func generatePDF() -> Data {
+    let pdfData = NSMutableData()
+    UIGraphicsBeginPDFContextToData(pdfData, CGRect(x: 0, y: 0, width: 612, height: 792), nil)
+    
+    guard let pdfContext = UIGraphicsGetCurrentContext() else { return Data() }
+    
+    UIGraphicsBeginPDFPage()
+    
+    let titleAttributes: [NSAttributedString.Key: Any] = [
+        .font: UIFont.boldSystemFont(ofSize: 18),
+        .foregroundColor: UIColor.black
+    ]
+    
+    let textAttributes: [NSAttributedString.Key: Any] = [
+        .font: UIFont.systemFont(ofSize: 12),
+        .foregroundColor: UIColor.black
+    ]
+    
+    let title = "Sample Title"
+    title.draw(at: CGPoint(x: 20, y: 20), withAttributes: titleAttributes)
+    
+    let text = "This is some sample content for the PDF."
+    text.draw(at: CGPoint(x: 20, y: 50), withAttributes: textAttributes)
+    
+    UIGraphicsEndPDFContext()
+    
+    return pdfData as Data
+}
+
+func displayPDF() {
+    let pdfData = generatePDF()
+    let pdfView = PDFView(frame: self.view.bounds)
+    pdfView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+    self.view.addSubview(pdfView)
+    
+    if let document = PDFDocument(data: pdfData) {
+        pdfView.document = document
+    }
+}
