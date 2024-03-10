442cbf4e739322a2631a1110eadaac16561d73d2
Zoya Garg <125168583+zoyagarg@users.noreply.github.com>
2024-03-09 16:14:27 -0800

    AH PLS SAVE ME

--- Intake/SocialHistory/SmokingHistory.swift
+++ Intake/SocialHistory/SmokingHistory.swift
@@ -33,25 +33,6 @@ struct SmokingHistoryView: View {
     
     
     @State private var hasSmoked: Bool? // swiftlint:disable:this discouraged_optional_boolean
-    struct YesNoButtonStyle: ButtonStyle {
-        var isSelected: Bool
-        
-        func makeBody(configuration: Self.Configuration) -> some View {
-            configuration.label
-                .padding()
-                .background(isSelected ? Color.blue : Color.white)
-                .foregroundColor(isSelected ? .white : .blue)
-                .cornerRadius(8)
-                .overlay(
-                    RoundedRectangle(cornerRadius: 8)
-                        .stroke(Color.blue, lineWidth: 2)
-                )
-                .scaleEffect(configuration.isPressed ? 0.95 : 1)
-        }
-    }
-    
-    
-    @State private var hasSmoked: Bool? // swiftlint:disable:this discouraged_optional_boolean
     @State private var daysPerYear: String = ""
     @State private var packsPerDay: String = ""
     @State private var packYears: Double = 0
