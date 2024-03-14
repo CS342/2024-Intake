6eabad55df71de3b9b74bf03501cbc160887af43
kcallon <kcallon@stanford.edu>
2024-03-14 12:26:56 -0700

    pls???

--- IntakeUITests/SurgeryTests.swift
+++ IntakeUITests/SurgeryTests.swift
@@ -48,11 +48,8 @@ class SurgeryTests: XCTestCase {
         app.textFields["SURGERY_STATUS"].tap()
         app.textFields["SURGERY_STATUS"].typeText("COMPLETED")
         app.navigationBars["New Surgery"].buttons["Surgical History"].tap()
-        XCTAssertTrue(app.staticTexts["Knee Surgery"].waitForExistence(timeout: 2))
         sleep(2)
-        app.buttons["Next"].tap()
-        sleep(3)
-        XCTAssertTrue(app.navigationBars["Patient Form"].waitForExistence(timeout: 2))
         XCTAssertTrue(app.staticTexts["Knee Surgery"].waitForExistence(timeout: 2))
+        app.buttons["Next"].tap()
     }
 }
