565910904d2ac49caa9817084ec2ab5087893bd3
Kate Callon <70660419+kcallon@users.noreply.github.com>
2024-03-13 20:59:07 -0700

    Adding Testing (#75)
    # *Initial Tests*
    
    ## :recycle: Current situation & Problem
    *Link any open issues or pull requests (PRs) related to this PR. Please
    ensure that all non-trivial PRs are first tracked and discussed in an
    existing GitHub issue or discussion.*
    
    
    ## :gear: Release Notes 
    *Add a bullet point list summary of the feature and possible migration
    guides if this is a breaking change so this section can be added to the
    release notes.*
    *Include code snippets that provide examples of the feature implemented
    or links to the documentation if it appends or changes the public
    interface.*
    
    
    ## :books: Documentation
    *Please ensure that you properly document any additions in conformance
    to [Spezi Documentation
    Guide](https://github.com/StanfordSpezi/.github/blob/main/DOCUMENTATIONGUIDE.md).*
    *You can use this section to describe your solution, but we encourage
    contributors to document your reasoning and changes using in-line
    documentation.*
    
    
    ## :white_check_mark: Testing
    *Please ensure that the PR meets the testing requirements set by CodeCov
    and that new functionality is appropriately tested.*
    *This section describes important information about the tests and why
    some elements might not be testable.*
    
    
    ## :pencil: Code of Conduct & Contributing Guidelines 
    
    By submitting creating this pull request, you agree to follow our [Code
    of
    Conduct](https://github.com/CS342/.github/blob/main/CODE_OF_CONDUCT.md)
    and [Contributing
    Guidelines](https://github.com/CS342/.github/blob/main/CONTRIBUTING.md):
    - [ ] I agree to follow the [Code of
    Conduct](https://github.com/CS342/.github/blob/main/CODE_OF_CONDUCT.md)
    and [Contributing
    Guidelines](https://github.com/CS342/.github/blob/main/CONTRIBUTING.md).

--- Intake/Medication View/IntakeMedicationInstance.swift
+++ Intake/Medication View/IntakeMedicationInstance.swift
@@ -15,6 +15,7 @@
 import Foundation
 import SpeziMedication
 
+// This defines an IntakeMedicationInstance which is composed of an id, an IntakeMedication type, a dosage, and a schedule. 
 struct IntakeMedicationInstance: MedicationInstance, MedicationInstanceInitializable, Codable {
     let id: UUID
     let type: IntakeMedication
--- Intake/Medication View/MedicationContentView.swift
+++ Intake/Medication View/MedicationContentView.swift
@@ -17,38 +17,49 @@ import SpeziFHIR
 import SpeziMedication
 import SwiftUI
 
+// This view displays the medications in the patient's FHIR data, and allows them to add, update and delete their medications.
 struct MedicationContentView: View {
     @Environment(FHIRStore.self) private var fhirStore
     @Environment(NavigationPathWrapper.self) private var navigationPath
     @Environment(DataStore.self) private var data
     @State private var presentSettings = false
-
+    
     @State private var medicationSettingsViewModel: IntakeMedicationSettingsViewModel?
-
+    
     var body: some View {
         VStack {
             if let medicationSettingsViewModel {
                 MedicationSettings(allowEmptySave: true, medicationSettingsViewModel: medicationSettingsViewModel) {
-                    data.medicationData = medicationSettingsViewModel.medicationInstances
-                    navigationPath.path.append(NavigationViews.allergies)
+                    if FeatureFlags.skipToScrollable {
+                        data.medicationData = medicationSettingsViewModel.medicationInstances
+                        navigationPath.path.append(NavigationViews.pdfs)
+                    } else {
+                        data.medicationData = medicationSettingsViewModel.medicationInstances
+                        navigationPath.path.append(NavigationViews.allergies)
+                    }
                 }
-                        .navigationTitle("Medications")
-                        .navigationBarItems(trailing: NavigationLink(destination: MedicationLLMAssistant(presentingAccount: .constant(false))) {
-                            Text("Chat")
-                        })
+                .navigationTitle("Medications")
+                .navigationBarItems(trailing: NavigationLink(destination: MedicationLLMAssistant(presentingAccount: .constant(false))) {
+                    Text("Chat")
+                })
             } else {
                 ProgressView()
             }
         }
-            .task {
-                let patientMedications = fhirStore.llmMedications
-                self.medicationSettingsViewModel = IntakeMedicationSettingsViewModel(existingMedications: patientMedications)
-                var initialData: Set<IntakeMedicationInstance> = []
-                if let newMed = self.medicationSettingsViewModel?.medicationInstances {
-                    initialData = newMed
-                }
-                data.medicationData = initialData
+        // Updates the medicationSettingsViewModel init if there's a change to the patient's fhirStore medications.
+        .onChange(of: fhirStore.llmMedications) {
+            medicationSettingsViewModel = .init(existingMedications: fhirStore.llmMedications)
+        }
+        // Task to initialize the MedicationSettingsViewModel with the patient's existing fhirStore medications.
+        .task {
+            let patientMedications = fhirStore.llmMedications
+            self.medicationSettingsViewModel = IntakeMedicationSettingsViewModel(existingMedications: patientMedications)
+            var initialData: Set<IntakeMedicationInstance> = []
+            if let newMed = self.medicationSettingsViewModel?.medicationInstances {
+                initialData = newMed
             }
+            data.medicationData = initialData
+        }
     }
     
     init() {}
